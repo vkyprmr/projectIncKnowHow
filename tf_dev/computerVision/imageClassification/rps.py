1a1ac18c6137c30a24d927a051e9cbd05bc6e4d8
vkyprmr <parmar.vickyk@gmail.com>
2020-10-08 21:10:26 +0200

    Create rps.py

---
+++ tf_dev/computerVision/imageClassification/rps.py
@@ -0,0 +1,13 @@
+"""
+Developer: vkyprmr
+Filename: rps.py
+Created on: 2020-10-8, Do., 17:17:41
+"""
+"""
+Modified by: vkyprmr
+Last modified on: 2020-10-8, Do., 17:19:44
+"""
+
+# ToDo: create a model for Rock-Paper-Scissors from scratch
+# ToDo: create a script to move 24 files from validation dir to test dir
+# ToDo: create a script for RPS implementing Transfer Learning
