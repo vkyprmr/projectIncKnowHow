d16fa0b3872737d05198092ba9ba54e47f497a3a
vkyprmr <parmar.vickyk@gmail.com>
2020-10-14 15:16:09 +0200

    added to do files

---
+++ tf_dev/nlp/textClassification/complex/shakespeare.py
@@ -0,0 +1,11 @@
+"""
+Developer: vkyprmr
+Filename: shakespeare.py
+Created on: 2020-10-14, Mi., 15:15:0
+"""
+"""
+Modified by: vkyprmr
+Last modified on: 2020-10-14, Mi., 15:15:1
+"""
+
+# ToDo: Try to build and understand character-based text generation
---
+++ tf_dev/nlp/textClassification/complex/song.py
@@ -0,0 +1,11 @@
+"""
+Developer: vkyprmr
+Filename: song.py
+Created on: 2020-10-14, Mi., 14:33:1
+"""
+"""
+Modified by: vkyprmr
+Last modified on: 2020-10-14, Mi., 14:33:2
+"""
+
+# ToDo: try to make a prediction model using any song.
